b46c502955407f5680c3b0b58264c0af1d937325
Alfred Godoy <alfred@nurd>
2021-09-11 08:19:03 +0100

    exclude gitignored pi-gen yada

--- stage6/00-pi-gen/01-run.sh
+++ stage6/00-pi-gen/01-run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rsync -a --exclude deploy --exclude work /opt/pi-gen/ "${ROOTFS_DIR}"/opt/pi-gen
+rsync -a --exclude-from /opt/pi-gen/.gitignore --exclude work --exclude deploy /opt/pi-gen/ "${ROOTFS_DIR}"/opt/pi-gen
