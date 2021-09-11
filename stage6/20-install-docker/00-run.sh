273598d46a165a71a67847f1fbfe3da55d09fd89
Alfred Godoy <alfred@nurd>
2021-09-11 08:35:52 +0100

    add docker

---
+++ stage6/20-install-docker/00-run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# install docker:
+curl -sSL https://get.docker.com | sh
+
+# enable docker daemon at boot:
+systemctl enable docker
+
+# add our user to the docker group:
+usermod -aG docker user
+
+# docker-compose:
+apt-get install libffi-dev libssl-dev
+apt install python3-dev
+apt-get install -y python3 python3-pip
+pip3 install docker-compose
+
