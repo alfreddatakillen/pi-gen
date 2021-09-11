9b06271e97419a19b10cfa8bbf2aed8a7e6846dd
Alfred Godoy <alfred@nurd.nu>
2021-09-11 22:10:14 +0100

    since config is in gitignore, it messed up .git repo when rsyncing. fixed

--- stage6/00-pi-gen/01-run.sh
+++ stage6/00-pi-gen/01-run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rsync -a --exclude-from /opt/pi-gen/.gitignore --exclude work --exclude deploy /opt/pi-gen/ "${ROOTFS_DIR}"/opt/pi-gen
+rsync -a --exclude work --exclude deploy /opt/pi-gen/ "${ROOTFS_DIR}"/opt/pi-gen
