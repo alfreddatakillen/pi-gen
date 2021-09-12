ae7b0db1ed7fcbb9092ef9bf7627407d717b3969
Alfred Godoy <alfred@nurd.nu>
2021-09-12 11:56:16 +0100

    fixes

--- stage6/10-auto-login/00-run-chroot.sh
+++ stage6/10-auto-login/00-run-chroot.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 systemctl set-default multi-user.target
 ln -fs /lib/systemd/system/getty@.service /etc/systemd/system/getty.target.wants/getty@tty1.service
-cat > /etc/systemd/system/getty@tty1.service.d/autologin.conf << EOF
+cat >/etc/systemd/system/getty@tty1.service.d/autologin.conf <<_EOF_
 [Service]
 ExecStart=
 ExecStart=-/sbin/agetty --autologin user --noclear %I \$TERM
-EOF
+_EOF_
