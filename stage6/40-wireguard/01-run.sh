2682f01ee81a7cc4c5c55c6e541ff2754a9acfb2
Alfred Godoy <alfred@nurd.nu>
2021-09-28 21:07:25 +0100

    fixes automatic wireguard at boot

--- stage6/40-wireguard/01-run.sh
+++ stage6/40-wireguard/01-run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-cp /etc/wireguard/wg-vpn.conf "${ROOTFS_DIR}"/etc/wireguard/wg-vpn.conf
-chmod 600 "${ROOTFS_DIR}"/etc/wireguard/wg-vpn.conf
+mkdir "${ROOTFS_DIR}"/etc/wireguard
+chmod 700 "${ROOTFS_DIR}"/etc/wireguard
+cp /etc/wireguard/wg0.conf "${ROOTFS_DIR}"/etc/wireguard/wg0.conf
+chmod 600 "${ROOTFS_DIR}"/etc/wireguard/wg0.conf
 
---
+++ stage6/40-wireguard/02-run-chroot.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+systemctl enable wg-quick@wg0.service
+
