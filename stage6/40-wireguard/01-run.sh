d79b9e3a0351cf6be36b70dec84652353c4bb722
Alfred Godoy <alfred@nurd.nu>
2021-10-03 10:24:21 +0100

    masquerading rules on wireguard interface up

--- stage6/40-wireguard/01-run.sh
+++ stage6/40-wireguard/01-run.sh
@@ -5,3 +5,20 @@ chmod 700 "${ROOTFS_DIR}"/etc/wireguard
 cp /etc/wireguard/wg0.conf "${ROOTFS_DIR}"/etc/wireguard/wg0.conf
 chmod 600 "${ROOTFS_DIR}"/etc/wireguard/wg0.conf
 
+cat <<_EOF_ >"${ROOTFS_DIR}"/etc/network/if-up.d/wg0nat
+#!/bin/bash
+[ "\$IFACE" = "wg0" ] || exit 0
+iptables -t nat -A POSTROUTING -o wg0 -j MASQUERADE
+iptables -A FORWARD -i eth1 -o wg0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i wg0 -o eth1 -j ACCEPT
+_EOF_
+chmod 755 "${ROOTFS_DIR}"/etc/network/if-up.d/wg0nat
+
+cat <<_EOF_ >"${ROOTFS_DIR}"/etc/network/if-down.d/wg0nat
+#!/bin/bash
+[ "\$IFACE" = "wg0" ] || exit 0
+iptables -D FORWARD -i wg0 -o eth1 -j ACCEPT
+iptables -D FORWARD -i eth1 -o wg0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -t nat -D POSTROUTING -o wg0 -j MASQUERADE
+_EOF_
+chmod 755 "${ROOTFS_DIR}"/etc/network/if-down.d/wg0nat
